9ea34e2632dcb7f76c8dd753fbd36e61908db3d5
Sooraj P R <19621059+soorajiam@users.noreply.github.com>
2024-09-22 22:17:31 +0100

    reqs

--- Paxable/settings/development.py
+++ Paxable/settings/development.py
@@ -13,13 +13,13 @@ STATICFILES_DIRS = [
 ]
 
 # Configure development database
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+# if DEVELOPMENT_MODE is True:
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
+}
 # elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
 #     if os.getenv("DATABASE_URL", None) is None:
 #         raise Exception("DATABASE_URL environment variable not defined")
